2e9e78f28bf5379a25039b001b27b935f29b589c
Tyler Nickerson <nickersoft@gmail.com>
2021-07-02 09:45:54 -0700

    Initial Commit

---
+++ Shared/NukeExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NukeExampleApp.swift
+//  Shared
+//
+//  Created by Tyler Nickerson on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct NukeExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
